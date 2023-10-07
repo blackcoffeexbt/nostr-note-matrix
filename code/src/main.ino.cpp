12cea5aa261915575eaf2c54fb7059443ebc601c
blackcoffeexbt <87530449+blackcoffeexbt@users.noreply.github.com>
2023-10-07 20:49:56 +0100

    Working

--- code/src/main.ino.cpp
+++ code/src/main.ino.cpp
@@ -1,3 +1,6 @@
+# 1 "/var/folders/96/v_l40xh56t74ymwrdnrq84d80000gn/T/tmp8otmpv38"
+#include <Arduino.h>
+# 1 "/Users/mark/projects/nostr-note-matrix/code/src/main.ino"
 #include <vector>
 #include <utility>
 #include <Arduino.h>
@@ -25,7 +28,7 @@
 
 #include <ArduinoJson.h>
 
-// freertos
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
@@ -33,26 +36,26 @@
 
 TaskHandle_t DisplayTaskHandle = NULL;
 
-#define PIN            32  // Define the pin where the data line is connected to
-#define MATRIX_WIDTH   32
-#define MATRIX_HEIGHT  8
+#define PIN 32
+#define MATRIX_WIDTH 32
+#define MATRIX_HEIGHT 8
 
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(MATRIX_WIDTH, MATRIX_HEIGHT, PIN,
-  NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
+  NEO_MATRIX_TOP + NEO_MATRIX_LEFT +
   NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
-  NEO_GRB            + NEO_KHZ800);
+  NEO_GRB + NEO_KHZ800);
 
 const uint16_t colors[] = {
   matrix.Color(255, 0, 0),
   matrix.Color(0, 255, 0),
   matrix.Color(0, 0, 255),
-  matrix.Color(255, 0, 255), // purple
-};  // Add more colors if you wish!
+  matrix.Color(255, 0, 255),
+};
 
-// declare a pubkey > username vector to use when creating the authors sub request
+
 std::vector<std::pair<String, String>> pubkeyUsernameVector;
 
-// Global message buffers shared by Serial and Scrolling functions
+
 #define BUF_SIZE 250
 char curMessage[BUF_SIZE] = {""};
 char newMessage[BUF_SIZE] = {""};
@@ -82,7 +85,7 @@ extern char relayString[80];
 fs::SPIFFSFS &FlashFS = SPIFFS;
 #define FORMAT_ON_FAIL true
 
-// define funcs
+
 void configureAccessPoint();
 void initWiFi();
 bool whileCP(void);
@@ -97,12 +100,19 @@ void loadSettings();
 void createNoteEventRequest();
 void connectToNostrRelays();
 
-#define BUTTON_PIN 0 // change this to the pin your button is connected to
-#define DOUBLE_TAP_DELAY 250 // delay for double tap in milliseconds
+#define BUTTON_PIN 0
+#define DOUBLE_TAP_DELAY 250
 
 volatile unsigned long lastButtonPress = 0;
 volatile bool doubleTapDetected = false;
-
+void IRAM_ATTR handleButtonInterrupt();
+void WiFiEvent(WiFiEvent_t event);
+void setNewMessage(const char *message);
+String getUsernameByPubkey(const String& pubkey);
+void setup();
+void loop();
+void DisplayTask(void * parameter);
+#line 106 "/Users/mark/projects/nostr-note-matrix/code/src/main.ino"
 void IRAM_ATTR handleButtonInterrupt() {
   unsigned long now = millis();
   if (now - lastButtonPress < DOUBLE_TAP_DELAY) {
@@ -111,85 +121,77 @@ void IRAM_ATTR handleButtonInterrupt() {
   lastButtonPress = now;
 }
 
-// Define the WiFi event callback function
+
 void WiFiEvent(WiFiEvent_t event) {
   switch(event) {
     case SYSTEM_EVENT_STA_GOT_IP:
       Serial.println("Connected to WiFi and got an IP");
-      connectToNostrRelays();      
+      connectToNostrRelays();
       break;
     case SYSTEM_EVENT_STA_DISCONNECTED:
       Serial.println("Disconnected from WiFi");
-      // WiFi.begin(ssid, password); // Try to reconnect after getting disconnected
+
       break;
   }
 }
 
 
-/**
- * @brief Create a Zap Event Request object
- * 
- */
+
+
+
+
 void createNoteEventRequest() {
-  // Create the REQ
+
   eventRequestOptions = new NostrRequestOptions();
-  // Populate kinds
+
   int kinds[] = {1};
   eventRequestOptions->kinds = kinds;
   eventRequestOptions->kinds_count = sizeof(kinds) / sizeof(kinds[0]);
 
-  // // Populate #p
-  // Serial.println("npubHexString is |" + String(npubHexString) + "|");
-  // if(String(npubHexString) != "") {
-  //   Serial.println("npub is specified");
+
+
+
+
     String* authors = new String[pubkeyUsernameVector.size()];
-    // add each pubkey to the authors array
+
     for (int i = 0; i < pubkeyUsernameVector.size(); i++) {
       authors[i] = pubkeyUsernameVector[i].first;
     }
     eventRequestOptions->authors = authors;
     eventRequestOptions->authors_count = pubkeyUsernameVector.size();
-  // }
+
 
   eventRequestOptions->limit = 20;
 
 
-  // We store this here for sending this request again if a socket reconnects
+
   serialisedEventRequest = "[\"REQ\", \"" + nostrRelayManager.getNewSubscriptionId() + "\"," + eventRequestOptions->toJson() + "]";
 
   delete eventRequestOptions;
 }
 
-/**
- * @brief Connect to the Nostr relays
- * 
- */
+
+
+
+
 void connectToNostrRelays() {
-  // first disconnect from all relays
+
   nostrRelayManager.disconnect();
   Serial.println("Requesting events");
 
-  // split relayString by comma into vector
+
   std::vector<String> relays;
   String relayStringCopy = String(relayString);
   int commaIndex = relayStringCopy.indexOf(",");
-  // while (commaIndex != -1) {
-  //   relays.push_back(relayStringCopy.substring(0, commaIndex));
-  //   relayStringCopy = relayStringCopy.substring(commaIndex + 1);
-  //   commaIndex = relayStringCopy.indexOf(",");
-  // }
-  // // add last item after last comma
-  // if (relayStringCopy.length() > 0) {
-  //   relays.push_back(relayStringCopy);
-  // }
+# 185 "/Users/mark/projects/nostr-note-matrix/code/src/main.ino"
   relays.push_back("nostr.mom");
 
-  // no need to convert to char* anymore
+
   nostr.setLogging(true);
   nostrRelayManager.setRelays(relays);
   nostrRelayManager.setMinRelaysAndTimeout(1,10000);
 
-  // Set some event specific callbacks here
+
   Serial.println("Setting callbacks");
   nostrRelayManager.setEventCallback("ok", okEvent);
   nostrRelayManager.setEventCallback("connected", relayConnectedEvent);
@@ -218,7 +220,7 @@ String lastPayload = "";
 void relayConnectedEvent(const std::string& key, const std::string& message) {
   socketDisconnectedCount = 0;
   Serial.println("Relay connected: ");
-  
+
   Serial.print(F("Requesting events:"));
   Serial.println(serialisedEventRequest);
 
@@ -228,16 +230,16 @@ void relayConnectedEvent(const std::string& key, const std::string& message) {
 void relayDisonnectedEvent(const std::string& key, const std::string& message) {
   Serial.println("Relay disconnected: ");
   socketDisconnectedCount++;
-  // reboot after 3 socketDisconnectedCount subsequenet messages
+
   if(socketDisconnectedCount >= 3) {
     Serial.println("Too many socket disconnections. Restarting");
-    // restart device
+
     ESP.restart();
   }
 }
 
 void okEvent(const std::string& key, const char* payload) {
-    if(lastPayload != payload) { // Prevent duplicate events from multiple relays triggering the same logic
+    if(lastPayload != payload) {
       lastPayload = payload;
       Serial.println("payload is: ");
       Serial.println(payload);
@@ -245,9 +247,9 @@ void okEvent(const std::string& key, const char* payload) {
 }
 
 void nip01Event(const std::string& key, const char* payload) {
-    if(lastPayload != payload) { // Prevent duplicate events from multiple relays triggering the same logic
+    if(lastPayload != payload) {
       lastPayload = payload;
-      // We can convert the payload to a StaticJsonDocument here and get the content
+
       StaticJsonDocument<1024> eventJson;
       deserializeJson(eventJson, payload);
       String pubkey = eventJson[2]["pubkey"].as<String>();
@@ -257,20 +259,20 @@ void nip01Event(const std::string& key, const char* payload) {
 }
 
 void setNewMessage(const char *message) {
-  int j = 0; // Index for newMessage
+  int j = 0;
   for (int i = 0; message[i] && j < BUF_SIZE - 1; i++) {
-    if (message[i] != '\n' && message[i] != '\r') { // Skip line breaks
+    if (message[i] != '\n' && message[i] != '\r') {
       newMessage[j++] = message[i];
     }
   }
-  newMessage[j] = '\0'; // Null-terminate the string
+  newMessage[j] = '\0';
 
   Serial.println("New message: " + String(newMessage));
   newMessageAvailable = true;
 }
 
 uint16_t getRandomNum(uint16_t min, uint16_t max) {
-  uint16_t rand  = (esp_random() % (max - min + 1)) + min;
+  uint16_t rand = (esp_random() % (max - min + 1)) + min;
   Serial.println("Random number: " + String(rand));
   return rand;
 }
@@ -281,16 +283,16 @@ String getUsernameByPubkey(const String& pubkey) {
             return pair.second;
         }
     }
-    return ""; // Return an empty string if pubkey not found
+    return "";
 }
 
 void noteEvent(const std::string& key, const char* payload) {
-    if(lastPayload != payload && !strstr(payload, "reply")) { // Prevent duplicate events from multiple relays triggering the same logic, as we are using multiple relays, this is likely to happen
+    if(lastPayload != payload && !strstr(payload, "reply")) {
       Serial.println("note payload is: ");
       Serial.println(payload);
-      // get content from note
+
       DynamicJsonDocument eventJson(20485);
-      // get err
+
       DeserializationError error = deserializeJson(eventJson, payload);
       if(error) {
         Serial.println("Error parsing json");
@@ -299,13 +301,13 @@ void noteEvent(const std::string& key, const char* payload) {
       }
       String content = eventJson[2]["content"].as<String>();
       Serial.println("content is: " + content);
-      // get the author pubkey from the pubkey key
+
       String authorPubkey = eventJson[2]["pubkey"].as<String>();
       Serial.println("author pubkey is: " + authorPubkey);
-      // lookup the username from the pubkey in pubkeyUsernameVector
+
       String username = getUsernameByPubkey(authorPubkey);
-      
-      // set the message
+
+
       if(username != "") {
         setNewMessage((username + " - " + content).c_str());
       } else {
@@ -323,7 +325,7 @@ void setup() {
   matrix.setBrightness(20);
   matrix.setTextColor(colors[3]);
 
-  // now add some data to pubkeyUsernameVector
+
 pubkeyUsernameVector.push_back(std::make_pair("50d94fc2d8580c682b071a542f8b1e31a200b0508bab95a33bef0855df281d63", "callebtc"));
 pubkeyUsernameVector.push_back(std::make_pair("04c915daefee38317fa734444acee390a8269fe5810b2241e5e6dd343dfbecc9", "ODELL"));
 pubkeyUsernameVector.push_back(std::make_pair("e9e4276490374a0daf7759fd5f475deff6ffb9b0fc5fa98c902b5f4b2fe3bba2", "benarc"));
@@ -331,20 +333,20 @@ pubkeyUsernameVector.push_back(std::make_pair("6e468422dfb74a5738702a8823b9b2816
 pubkeyUsernameVector.push_back(std::make_pair("683211bd155c7b764e4b99ba263a151d81209be7a566a2bb1971dc1bbd3b715e", "BlackCoffee"));
 
   FlashFS.begin(FORMAT_ON_FAIL);
-  // init spiffs
+
   if(!SPIFFS.begin(true)){
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
   }
 
   xTaskCreatePinnedToCore(
-    DisplayTask,               /* Task function */
-    "DisplayTask",             /* Task name */
-    10000,                     /* Stack depth (increase if you face any stack overflow issues) */
-    NULL,                      /* Parameters for the task function */
-    1,                         /* Priority */
-    &DisplayTaskHandle,        /* Task handle */
-    1                          /* Core where the task should run */
+    DisplayTask,
+    "DisplayTask",
+    10000,
+    NULL,
+    1,
+    &DisplayTaskHandle,
+    1
   );
 
   if (DisplayTaskHandle == NULL){
@@ -353,10 +355,10 @@ pubkeyUsernameVector.push_back(std::make_pair("683211bd155c7b764e4b99ba263a151d8
 
     setNewMessage("Nostr");
 
-  randomSeed(analogRead(0)); // Seed the random number generator
+  randomSeed(analogRead(0));
 
-  // delay(500);
-  // signalWithLightning(2,250);
+
+
 
   WiFi.onEvent(WiFiEvent);
   init_WifiManager();
@@ -377,12 +379,12 @@ bool lastInternetConnectionCheckTime = 0;
 void loop() {
   if (millis() - lastInternetConnectionCheckTime > 10000) {
     if(WiFi.status() == WL_CONNECTED) {
-      IPAddress ip(9,9,9,9);  // Quad9 DNS
+      IPAddress ip(9,9,9,9);
       bool ret = Ping.ping(ip);
       if(ret) {
         if(!lastInternetConnectionState) {
           Serial.println("Internet connection has come back! :D");
-          // reboot
+
           ESP.restart();
         }
         lastInternetConnectionState = true;
@@ -395,7 +397,7 @@ void loop() {
   nostrRelayManager.loop();
   nostrRelayManager.broadcastEvents();
 
-  // reboot every hour
+
   if (millis() > 3600000) {
     Serial.println("Rebooting");
     ESP.restart();
@@ -405,7 +407,7 @@ void loop() {
 
 bool hasFinishedAnimating = false;
 
-// The task function
+
 void DisplayTask(void * parameter) {
   while(1) {
     if (newMessageAvailable) {
@@ -425,24 +427,7 @@ void DisplayTask(void * parameter) {
       hasFinishedAnimating = true;
     }
     matrix.show();
-    delay(40);  // Adjust speed
+    delay(40);
   }
-
-  // while(1) { // Keep running this in a loop
-  //   hasFinishedAnimating = P.displayAnimate();
-    
-
-  //   if (hasFinishedAnimating) {
-  //     Serial.println("Finished animating");
-      
-  //     if (newMessageAvailable)
-  //     {
-  //       strcpy(curMessage, newMessage);
-  //     }
-  //     P.displayReset();
-  //     newMessageAvailable = false;
-  //   }
-  //   // Optional delay to give other tasks time (if necessary)
-  //   // vTaskDelay(pdMS_TO_TICKS(10));
-  // }
+# 448 "/Users/mark/projects/nostr-note-matrix/code/src/main.ino"
 }
